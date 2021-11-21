d325b1c6495ee1affa9ee3095c2cfc88aff50212
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-11-21 16:49:39 +0330

    added move controller to paddles

---
+++ Assets/Scripts/PaddleConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "PaddleConfig")]
+public class PaddleConfig : ScriptableObject
+{
+    public string name;
+    public float direction;
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    [SerializeField] private PaddleConfig config;
+
+    private static Vector3 _direction;
+
+    [SerializeField] private float movementAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _direction = new Vector3(  movementAmount,0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(_direction * Time.deltaTime * config.direction);
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(-_direction * Time.deltaTime * config.direction);
+    }
+}
