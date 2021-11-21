4b3f45682aa9848f8314e43318c84d4581c528f6
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-11-21 18:20:23 +0330

    fixed ui element configs

---
+++ Assets/Scripts/CustomEventSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class MyIntEvent : UnityEvent<int> {}
+[System.Serializable]
+public class MyTextEvent : UnityEvent<string> {}
+
+
+public class CustomEventSystem : MonoBehaviour
+{
+    public static CustomEventSystem current;
+    public MyIntEvent onFoodCollect;
+    public MyIntEvent onScoreChange;
+    public UnityEvent onEndGame;
+
+    void Awake()
+    {
+        current = this;
+        onFoodCollect = new MyIntEvent();
+        onScoreChange = new MyIntEvent();
+        onEndGame = new UnityEvent();
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Text leftScore;
+    [SerializeField] Text rightScore;
+    [SerializeField] Text endGame;
+
+    private int _leftScore;
+    private int _rightScore;
+    // [SerializeField] EventSystemCustom eventSystemCustom;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _leftScore = 0;
+        _rightScore = 0;
+        CustomEventSystem.current.onEndGame.AddListener(UpdateEndGame);
+        CustomEventSystem.current.onScoreChange.AddListener(UpdateScore);
+        UpdateScore(0);
+
+    }
+    private void UpdateScore(int addedScore)
+    {
+
+    }
+    private void UpdateEndGame()
+    {
+        endGame.text = "You lose!\nYour Score is ";
+    }
+}
