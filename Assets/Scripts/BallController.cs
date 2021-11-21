ac0ec6309803bd510df9a32e8e2acca34546dc24
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-11-21 17:09:51 +0330

    added movement to ball

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRandomPlace();
+        SetRandomVelocity();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetRandomPlace()
+    {
+        transform.position = new Vector3(0f, Random.Range(-4, 4), 0f);
+    }
+
+    private void SetRandomVelocity()
+    {
+        float velX = Random.Range(0.5f, 1f);
+        Debug.Log(velX);
+        float velY = math.sqrt(1 - velX * velX);
+        Vector3 vel = new Vector3(velX, velY, 0f);
+        Debug.Log(vel);
+        GetComponent<Rigidbody2D>().velocity = velocity * vel;
+    }
+}
